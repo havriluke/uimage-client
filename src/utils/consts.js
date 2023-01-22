19fa97d7193cf9d1da5ae5867b445c295567743c
havriluke <havrylyuk.post@gmail.com>
2023-01-23 01:05:55 +0200

    change url

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -6,5 +6,8 @@ export const ABOUT_ROUTE = '/about'
 export const PREMIUM_ROUTE = '/premium'
 export const API_ROUTE = '/api'
 
-export const CLIENT_URL = 'http://localhost:3000/'
-export const API_URL = 'http://localhost:5000/'
+export const CLIENT_URL = 'https://uimage.onrender.com/'
+export const API_URL = 'https://uimage-server.onrender.com/'
+
+//https://uimage-server.onrender.com
+//http://localhost:5000/
