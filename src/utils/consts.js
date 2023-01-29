41acc080a2265a0148a66e5e7fcf37d43c0a9d98
havriluke <havrylyuk.post@gmail.com>
2023-01-29 14:07:17 +0200

    mobile fix 1

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -6,8 +6,8 @@ export const ABOUT_ROUTE = '/about'
 export const PREMIUM_ROUTE = '/premium'
 export const API_ROUTE = '/api'
 
-export const CLIENT_URL = 'https://uimage.onrender.com/'
-export const API_URL = 'https://uimage-server.onrender.com/'
+export const CLIENT_URL = 'https://uimage.onrender.com'
+export const API_URL = 'https://uimage-server.onrender.com'
 
 //https://uimage-server.onrender.com
 //http://localhost:5000/
